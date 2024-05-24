5b8b7893a008889e65237c3d1a6fd2256d28fc7a
Lison <lisonfortin@icloud.com>
2024-05-24 09:59:49 +0200

    correction Wave
    fin creation wave level 1, et mise en constante 10 le nombre d'ennemis par Wave

--- src/wave.cpp
+++ src/wave.cpp
@@ -5,14 +5,22 @@
 
 void Wave::create_wave(int level){
     if(level == 1){
-        for(int i {0}; i<10; i++){
-            Enemy one_enemy {1+i, 150, 2, 20, Enemy_Type::Normal};
+        for(int i {0}; i<this->number_enemys; i++){
+            Enemy one_enemy {1+i, 150, 2, 20, Enemy_Type::Normal, 0, 0};
             if(i==3){
-                one_enemy->life = 100;
-                one_enemy->speed = 3;
-                one_enemy->money_reward = 30;
+                one_enemy.life = 100;
+                one_enemy.speed = 3;
+                one_enemy.money_reward = 30;
+                one_enemy.type = Enemy_Type::Fast;
             }
-            
+            else if(i==8){
+                one_enemy.life = 250;
+                one_enemy.speed = 1;
+                one_enemy.money_reward = 50;
+                one_enemy.type = Enemy_Type::Robust;
+            }
+
+            this->enemys.push_back(one_enemy);
         }
     }
 }
--- src/wave.hpp
+++ src/wave.hpp
@@ -3,7 +3,7 @@
 
 struct Wave{
     std::vector<Enemy> enemys {};
-    int number_enemys;
+    int number_enemys {10};
 
     void create_wave(int level); // Doit prendre en paramètres le niveau dans lequel on est pour créer le tableau d'Enemys
 };
