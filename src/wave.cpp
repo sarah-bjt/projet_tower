5a32df7ccdc98a900eef5175b8f79c7dd0799dd7
Lison <lisonfortin@icloud.com>
2024-06-07 16:26:32 +0200

    commentaires wave

--- src/wave.cpp
+++ src/wave.cpp
@@ -18,8 +18,8 @@ double previous_time {0.0};
 void Wave::create_wave(int level){
     if(level == 1){
         for(int i {0}; i<this->number_enemys; i++){
-            Enemy one_enemy {1+i, 150, 0.5f, 20, Enemy_Type::Normal, 0.99f-(((float)i)*0.2f), 0.4f};
-            if(i==3){
+            Enemy one_enemy {1+i, 150, 0.5f, 20, Enemy_Type::Normal, 0.99f-(((float)i)*0.2f), 0.4f};  //initialise l'ennemis
+            if(i==3){  //condition temporaire pour intégrer des types d'ennemis différents dans une même vague
                 one_enemy.life = 100;
                 one_enemy.speed = 1.0f;
                 one_enemy.money_reward = 30;
@@ -32,7 +32,7 @@ void Wave::create_wave(int level){
                 one_enemy.type = Enemy_Type::Robust;
             }
 
-            this->enemys.push_back(one_enemy);
+            this->enemys.push_back(one_enemy);  //ajout de l'ennemi dans le tableau d'ennemis de la vague
         }
     }
     else if(level == 2){
@@ -52,13 +52,13 @@ void Wave::wave_setup(){
 
 void Wave::wave_forward(bool horizontal, bool vertical, bool UP, bool DOWN, bool LEFT, bool RIGHT){
     const double time {glfwGetTime()/10};
-    const double time_elapse {time - previous_time};
+    const double time_elapse {time - previous_time};  //récupère le temps en direct
     previous_time = time;
-    if(horizontal && !vertical){
+    if(horizontal && !vertical){   // Vérifie les directions demandées pour toute la vague d'ennemis
         if(RIGHT && !LEFT){
             for(int i {0}; i<this->number_enemys; i++){
-                this->enemys[i].pos_X+=this->enemys[i].speed*time_elapse;
-                this->enemys[i].enemy_apparition();
+                this->enemys[i].pos_X+=this->enemys[i].speed*time_elapse;  //modifie la position de l'ennemi directement en ajoutant le temps direct multiplié par la vitesse de l'ennemi.
+                this->enemys[i].enemy_apparition();  //Apelle la méthode d'apparition de l'ennemi
             }
         }
         else if(!RIGHT && LEFT){
