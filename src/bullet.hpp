4a5c7f40d18c15298030a26ca9de0e63eb0f7aca
Lison <lisonfortin@icloud.com>
2024-06-08 20:30:42 +0200

    implémentation de méthodes pour bullet
    DEBUT

--- src/bullet.hpp
+++ src/bullet.hpp
@@ -3,5 +3,9 @@
 struct Bullet{
     int size;  //Taille de la bullet
     float speed;
+    //une instance qui contiendra le dessin de la balle
 
+    void bullet_draw(); //méthode pour dessiner la balle
+    void bullet_fire(float pos_X, float pos_Y); //méthode qui récupère la position/direction de l'ennemi et fais bouger la balle
+    void bullet_disappear();  //méthode qui contiendra la condition que la position soit atteinte pour faire disparaitre le dessin
 };
