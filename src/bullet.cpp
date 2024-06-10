b9eb1d8be455223949d4323dc30dc9ed562a6c9a
Lison <lisonfortin@icloud.com>
2024-06-10 12:35:28 +0200

    C'EST BON BULLET_FIRE S'ADAPTE !!!!!!
    du coup on peut tirer des balles dans tous les sens hehe

--- src/bullet.cpp
+++ src/bullet.cpp
@@ -26,8 +26,8 @@ void Bullet::bullet_draw(){
     glVertex2f(this->X, this->Y);
     for (int i = 0; i <= 100; i++) {
         float angle = 2.0f * M_PI * float(i) / float(100);
-        float x = cosf(angle) * this->size;
-        float y = sinf(angle) * this->size;
+        float x = cosf(angle) * this->size +  this->X;
+        float y = sinf(angle) * this->size +  this->Y;
         
         glVertex2f(x, y);
     }
@@ -39,8 +39,8 @@ void Bullet::bullet_draw(){
 void Bullet::bullet_fire(float target_X, float target_Y){
     const double time {glfwGetTime()/10};
 
-    this->_directX += target_X * this->speed * time;
-    this->_directY += target_Y * this->speed * time;
+    this->_directX += (target_X - this->X) * this->speed * time;
+    this->_directY += (target_Y - this->Y) * this->speed * time;
 
     //std::cout << "_directX = " << this->_directX << " et _directY = " << this->_directY << std::endl;
 
