97ebbcaa86eb87696d756bf6e7e4a48f40a8b1ee
Lison <lisonfortin@icloud.com>
2024-06-15 19:34:09 +0200

    pour enregistrement
    j'ai mis en commentaire tous player et tous party car il y avait des problèmes

--- src/party.cpp
+++ src/party.cpp
@@ -1,49 +1,49 @@
-#include "party.hpp"
-#include <algorithm>
+// #include "party.hpp"
+// #include <algorithm>
 
-Shop::Shop()
-{
-    TowersToBuy["Wood"] = Tower{0, 10, 5, 1.0f, 50, Tower_Type::Wood};
-    TowersToBuy["Rock"] = Tower{1, 20, 7, 2.0f, 75, Tower_Type::Rock};
-}
+// Shop::Shop()
+// {
+//     TowersToBuy["Wood"] = Tower{0, 10, 5, 1.0f, 50, Tower_Type::Wood};
+//     TowersToBuy["Rock"] = Tower{1, 20, 7, 2.0f, 75, Tower_Type::Rock};
+// }
 
-void Shop::displayItems() const
-{
-    std::cout << "Tours en vente : " << std::endl;
-    for (const auto& pair : TowersToBuy)
-    {
-        const auto& tower = pair.second;
-        std::cout << "Nom : " << pair.first 
-                  << ", Type : " << static_cast<int>(tower.type) 
-                  << ", Puissance : " << tower.fire_power 
-                  << ", Distance : " << tower.fire_distance 
-                  << ", Rythme : " << tower.fire_rythm 
-                  << ", Prix : " << tower.price 
-                  << std::endl;
-    }
-}
+// void Shop::displayItems() const
+// {
+//     std::cout << "Tours en vente : " << std::endl;
+//     for (const auto& pair : TowersToBuy)
+//     {
+//         const auto& tower = pair.second;
+//         std::cout << "Nom : " << pair.first 
+//                   << ", Type : " << static_cast<int>(tower.type) 
+//                   << ", Puissance : " << tower.fire_power 
+//                   << ", Distance : " << tower.fire_distance 
+//                   << ", Rythme : " << tower.fire_rythm 
+//                   << ", Prix : " << tower.price 
+//                   << std::endl;
+//     }
+// }
 
-void Shop::buyItems(Player& player)
-{
-    displayItems();
+// void Shop::buyItems(Player& player)
+// {
+//     displayItems();
 
-    int itemName;
-    std::cout << "Entrez le nom de la tour désirée : " << std::endl;
-    std::cin.ignore();
-    std::getline(std::cin, itemName);
+//     int itemName;
+//     std::cout << "Entrez le nom de la tour désirée : " << std::endl;
+//     std::cin.ignore();
+//     std::getline(std::cin, itemName);
 
-    auto it = TowersToBuy.find(itemName);
-    if (it == TowersToBuy.end())
-    {
-        std::cout << "Tour non trouvée." << std::endl;
-        return;
-    }
+//     auto it = TowersToBuy.find(itemName);
+//     if (it == TowersToBuy.end())
+//     {
+//         std::cout << "Tour non trouvée." << std::endl;
+//         return;
+//     }
 
-    Tower& selectedTower = it -> second;
+//     Tower& selectedTower = it -> second;
 
-    if (player.buy(selectedTower.price))
-    {
-        player.addTower(selectedTower);
-        std::cout << "Achat terminé. Vous avez acheté : " << itemName << std::endl;
-    }
-}
+//     if (player.buy(selectedTower.price))
+//     {
+//         player.addTower(selectedTower);
+//         std::cout << "Achat terminé. Vous avez acheté : " << itemName << std::endl;
+//     }
+// }
