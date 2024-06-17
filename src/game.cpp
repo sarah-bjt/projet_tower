9050f097ad0426fa0a1f77366abf3b49d113570b
Lison <lisonfortin@icloud.com>
2024-06-18 00:09:36 +0200

    limite d'achat/argent on est plus a decouvert YESAI

--- src/game.cpp
+++ src/game.cpp
@@ -39,23 +39,33 @@ void Game::update(int player_action, std::pair<double, double> mouse_position)
 
     if (player_action == 1 && !woodTowerPurchased) //Pour tower Wood
     {
-        std::cout << "achat tour B dans les positions "<<mouse_position.first<<mouse_position.second << std::endl;
-        Tower towerW = Tower {1, 20, 2, 0.0005, 60, Tower_Type::Wood,position_tower_x, position_tower_y};
-        this->player.decreaseMoney(towerW.price);
-        all_towers.push_back(towerW);
-        std::cout << "argent = " << this->player.money << std::endl;
-        woodTowerPurchased = true;
-        std::cout << "Nombre total de tours : " << all_towers.size() << std::endl;
+        if(this->player.money-60 >= 0){
+            std::cout << "achat tour B dans les positions "<<mouse_position.first<<mouse_position.second << std::endl;
+            Tower towerW = Tower {1, 20, 2, 0.0005, 60, Tower_Type::Wood,position_tower_x, position_tower_y};
+            this->player.decreaseMoney(towerW.price);
+            all_towers.push_back(towerW);
+            //std::cout << "argent = " << this->player.money << std::endl;
+            woodTowerPurchased = true;
+            std::cout << "Nombre total de tours : " << all_towers.size() << std::endl;
+        }
+        else {
+            std::cout << "Vous n'avez pas assez d'argent" << std::endl;
+        }
     }
     else if (player_action == 2 && !rockTowerPurchased) // Pour tower Rock
     {
-        std::cout << "achat tour P dans les positions "<<mouse_position.first<<mouse_position.second << std::endl;
-        Tower towerR = Tower {1, 80, 3, 0.0005, 150, Tower_Type::Rock,position_tower_x, position_tower_y};
-        this->player.decreaseMoney(towerR.price);
-        all_towers.push_back(towerR);
-        std::cout << "argent = " << this->player.money << std::endl;
-        rockTowerPurchased = true;
-        std::cout << "Nombre total de tours : " << all_towers.size() << std::endl;
+        if(this->player.money-150 >= 0){
+            std::cout << "achat tour P dans les positions "<<mouse_position.first<<mouse_position.second << std::endl;
+            Tower towerR = Tower {1, 80, 3, 0.0005, 150, Tower_Type::Rock,position_tower_x, position_tower_y};
+            this->player.decreaseMoney(towerR.price);
+            all_towers.push_back(towerR);
+            //std::cout << "argent = " << this->player.money << std::endl;
+            rockTowerPurchased = true;
+            std::cout << "Nombre total de tours : " << all_towers.size() << std::endl;        
+        }
+        else {
+            std::cout << "Vous n'avez pas assez d'argent" << std::endl;
+        }
     }
     if (player_action == 0)
     {
