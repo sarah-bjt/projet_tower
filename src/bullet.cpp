7d6dad6de15e0560e2e143fd5217ac02e69b12f0
LisonKas <151620502+LisonKas@users.noreply.github.com>
2024-06-18 16:31:59 +0200

    bullet.cpp nettoyé

--- src/bullet.cpp
+++ src/bullet.cpp
@@ -15,31 +15,31 @@
 
 #include "enemy.hpp"
 
-float invisible {1.0f};
+// float invisible {1.0f};
 double previous_time_time {0.0};
 
-void Bullet::bullet_draw(){
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+// void Bullet::bullet_draw(){
+//     glMatrixMode(GL_MODELVIEW);
+//     glLoadIdentity();
+//     glEnable(GL_BLEND);
+//     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    glPushMatrix();
-    glTranslatef(this->_directX, this->_directY, 0.0f);
-    glColor4f(0.0f, 0.0f, 0.0f, invisible); //invisible
-    glBegin(GL_TRIANGLE_FAN);
-    glVertex2f(this->X, this->Y);
-    for (int i = 0; i <= 100; i++) {
-        float angle = 2.0f * M_PI * float(i) / float(100);
-        float x = cosf(angle) * this->size +  this->X;
-        float y = sinf(angle) * this->size +  this->Y;
+//     glPushMatrix();
+//     glTranslatef(this->_directX, this->_directY, 0.0f);
+//     glColor4f(0.0f, 0.0f, 0.0f, invisible); //invisible
+//     glBegin(GL_TRIANGLE_FAN);
+//     glVertex2f(this->X, this->Y);
+//     for (int i = 0; i <= 100; i++) {
+//         float angle = 2.0f * M_PI * float(i) / float(100);
+//         float x = cosf(angle) * this->size +  this->X;
+//         float y = sinf(angle) * this->size +  this->Y;
         
-        glVertex2f(x, y);
-    }
+//         glVertex2f(x, y);
+//     }
     
-    glEnd();
-    glPopMatrix();
-}
+//     glEnd();
+//     glPopMatrix();
+// }
 
 void Bullet::bullet_fire(Enemy& enemy, int power_impact){
     while((this->X > enemy.pos_X+(120.0f/720.0f) && this->X < enemy.pos_X) && (this->Y > enemy.pos_Y+(120.0f/720.0f) && this->Y < enemy.pos_Y)){
@@ -47,19 +47,18 @@ void Bullet::bullet_fire(Enemy& enemy, int power_impact){
         const double time_elapsed2 {time - previous_time_time}; 
         previous_time_time = time;
 
-        this->_directX += (enemy.pos_X - this->X) * this->speed * time_elapsed2;
+        this->_directX += (enemy.pos_X - this->X) * this->speed * time_elapsed2;  //On calcule les nouvelles positions de la balle en fonction du temps et de sa vitesse mais aussi de la position de l'ennemi renseigné dans l'appel de la fonction
         this->_directY += (enemy.pos_Y - this->Y) * this->speed * time_elapsed2;
 
-        this->bullet_draw();
+        // this->bullet_draw();
 
-        this->X = this->_directX;
+        this->X = this->_directX;  //On met à jour la position de la bullet
         this->Y = this->_directY;
     }
-    std::cout << "balle disparait" << std::endl;
-    enemy.life -= power_impact;
-    this->bullet_disappear();
+    enemy.life -= power_impact;  //une fois que la bullet a atteint son but, on enlève de la vie sur les ennemis. On a récupéré la puissance de l'impact dans l'appel de la fonction
+    // this->bullet_disappear();
 }
 
-void Bullet::bullet_disappear(){
-    invisible = 0.0f;
-}
+// void Bullet::bullet_disappear(){
+//     invisible = 0.0f;
+// }
