ff38072a335299402fee2fc3f8ad4875551e5772
LisonKas <151620502+LisonKas@users.noreply.github.com>
2024-06-18 17:37:38 +0200

    wave.hpp entièrement nettoyé

--- src/wave.hpp
+++ src/wave.hpp
@@ -6,15 +6,13 @@
 #include <vector>
 
 struct Wave{
-    int number_enemys;
-    std::vector<Enemy> enemys {};
-    WeightedGraph graph;
-
-    //Il va me falloir une instance Path pour pouvoir gérer la direction de la vague, et ainsi faire tourner les ennemis un par un
+    int number_enemys;  //indique le nombre d'ennemis que contiendra la vague
+    std::vector<Enemy> enemys {};  //Contient tous les ennemis de la vague
+    WeightedGraph graph;  //Contient le graphe de la map
 
     void create_wave(int level); // Doit prendre en paramètres le niveau dans lequel on est pour créer le tableau d'Enemys
-    void wave_forward(); // Méthode qui fait avancer le tableau d'ennemis   //bool horizontal, bool vertical, bool UP, bool DOWN, bool LEFT, bool RIGHT
+    void wave_forward(); // Méthode qui fait avancer le tableau d'ennemis 
     void wave_setup(int level); // méthode pour mettre en place la wave
-    bool enemys_of_one_wave_dead();
+    bool enemys_of_one_wave_dead(); //Méthode pour savoir si tous les ennemis de la vague sont mort sans lancer le message WIN
     void allEnemiesDead() const; //savoir si tous les ennemis sont mort 
 };
